bb97346dc03d26478510057acf7666f999278a46
kelllyy101 <>
2023-07-24 10:20:01 +0000

    Add user expenses categories and unicode emojis

--- run.py
+++ run.py
@@ -2,16 +2,23 @@ def main():
     print(f"Running Expense Tracker!")
     get_users_expense()
     save_expense_to_file()
-    summarise_expense()
+    summarise_expenses()
 
 def get_users_expense():
     print("Getting Users Expense")
-    expense_name = input("Enter expense name:")
-    print()
+    expense_category = input("Enter expense name: ")
+    expense_amount = float(input("Enter expense amount: "))
+    print(f"You've entered {expense_category}, {expense_amount}")
+
+    expense_categories = [
+        "🍔 Food", "🏠 House", "🚇 Trasport", "💊 Health", "🚙 Car", "👕 Clothes", "🎉 Fun", "💸 Bills", "🐶 Pets", "🍴 Restaurants"
+    ]
+
+    while True:
+        print("Select a category: ")
 
 def save_expense_to_file():
     print(f"Saving User Expense")
-
 
 def summarise_expenses():
     print(f"Summarising User Expense")
