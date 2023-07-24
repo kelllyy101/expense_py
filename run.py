6f624ff98a97a52dc67855b35d363166fa7a3360
kelllyy101 <>
2023-07-24 09:57:49 +0000

    main structure and functions added

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+    print(f"Running Expense Tracker!")
+    get_users_expense()
+    save_expense_to_file()
+    summarise_expense()
+
+def get_users_expense():
+    print("Getting Users Expense")
+    expense_name = input("Enter expense name:")
+    print()
+
+def save_expense_to_file():
+    print(f"Saving User Expense")
+
+
+def summarise_expenses():
+    print(f"Summarising User Expense")
+
+if __name__ =="__main__":
+    main()
